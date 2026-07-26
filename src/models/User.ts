c4b58b7617b1c9a76a02849ed4c6da8132186ca6
WissemJderi <asqr43641@gmail.com>
2026-07-26 09:05:37 +0100

    create user schema

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+export default model<IUser>("User", userSchema);
